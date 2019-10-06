804e1498a3c59d53182c7f425cb4cb6ffa39ba20
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-05 23:38:00 -0400

    Added fill tree function to populate binary tree

--- main.c
+++ main.c
@@ -1,1 +1,39 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "time.h"
 
+struct binaryTree
+{
+    struct binaryTree *left;
+    struct binaryTree *right;
+    int key;
+};
+
+//function that fills a binary tree with the amount of nodes specified in the function call
+struct binaryTree fillTree(struct binaryTree *node, int numNodes)
+{
+    if(numNodes <= 0)
+    {
+        return;
+    }
+
+    else
+    {
+        node = malloc(sizeof(struct binaryTree));
+        printf("In else");
+        node->key = rand() % 100;
+        fillTree(node->left, numNodes-1);
+        fillTree(node->right, numNodes-1);
+    }
+
+
+}
+
+int main()
+{
+    srand(time(0));
+    struct binaryTree *node;
+    fillTree(node, 5);
+    printf("After fill tree");
+
+}
