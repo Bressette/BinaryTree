d0e278d13b9abe1e1e1edbe27ff52e604df4c1f9
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-06 13:48:20 -0400

    Remove fillTree() add inOrder() and newNode()
    Removed fillTree function and added inOrder function that uses recursion
    to print the inOrder traversal of the binary tree that is passed into
    the function. newNode function sets the int value passed into the
    function to the node and sets the left and right values to NULL. Also
    added a visual representation of the binary tree using ascii art in a
    comment.
    
    The fillTree function was removed as it was not useful to have a binary
    tree filled with random values for testing the binary tree traversal
    algorithms. The newNode function was added to enable manual creation of
    nodes with specific values. The inOrder function was added to progress
    through the tree in order from left to root to right and print the value
    in each node.

--- main.c
+++ main.c
@@ -9,44 +9,52 @@ struct binaryTree
     int key;
 };
 
-//function that fills a binary tree with the amount of nodes specified in the function call
-
-
-
-struct binaryTree *fillTree(struct binaryTree *node, int numNodes)
-
-{
-    if(numNodes <= 0)
-    {
-        return;
-    }
-    else
-    {
-        node = malloc(sizeof(struct binaryTree));
-        node->key = rand() % 100;
-        fillTree(node->left, numNodes-1);
-        fillTree(node->right, numNodes-1);
-    }
-
-}
-
+/*function that prints the inOrder traversal of the binary tree
+ *in which first the left side of the tree is printed followed by the root
+ *and then the right side of the tree
+*/
 void inOrder(struct binaryTree *node)
 {
-    if(node->key == NULL)
+    if(node == NULL)
         return;
     else
     {
         inOrder(node->left);
-        printf("The value of key is %d", node->key);
+        printf("%d ", node->key);
         inOrder(node->right);
     }
 
 }
 
+struct binaryTree *newNode(int data)
+{
+    struct binaryTree *node = malloc(sizeof(struct binaryTree));
+    node->key = data;
+    node->left = NULL;
+    node->right = NULL;
+    return(node);
+}
+
 int main()
 {
     srand(time(0));
-    struct binaryTree *node;
-    node = fillTree(node, 5);
-    fillTree(node, 5);
+    struct binaryTree *node = newNode(1);
+    node->left = newNode(2);
+    node->right = newNode(3);
+    node->left->left = newNode(4);
+    node->left->right = newNode(5);
+    node->right->left = newNode(6);
+    node->right->right = newNode(7);
+    inOrder(node);
+    printf("\n");
+
 }
+
+/*The current binary tree looks like the following:
+                1
+            /       \
+           2         3
+         /   \      /  \
+        4     5    6    7
+*/
+
